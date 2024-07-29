df1511b3d3f06f6d13041ccdaf7f10abe29957ab
junnushon <junnu.shon@gmail.com>
2024-07-29 18:58:55 +0900

    debug 3peer issue 2

--- static/room.js
+++ static/room.js
@@ -23,7 +23,7 @@ let remotePeers = []; // 방에 있는 다른 사용자들의 ID를 저장
 let addedIceCandidates = {}; // 추가된 ICE 후보를 저장
 let pendingIceCandidates = {}; // 대기 중인 ICE 후보를 저장
 console.log('userId:', userId)
-console.log('App version: 1.0.7');
+console.log('App version: 1.0.8');
 
 document.addEventListener('DOMContentLoaded', async () => {
     await fetchRoomTitle();
@@ -69,7 +69,6 @@ async function handleIceCandidate(peerId, candidate) {
 
     addedIceCandidates[peerId].add(candidateId);
 
-    // 연결 상태가 'stable'인 경우, ICE 후보를 처리하지 않음
     if (pcs[peerId] && pcs[peerId].iceConnectionState === 'connected') {
         console.log(`Peer ${peerId} is already connected. Ignoring ICE candidate.`);
         return;
@@ -93,6 +92,7 @@ async function handleIceCandidate(peerId, candidate) {
 
 
 
+
 async function handleRemoteDescription(peerId, sdp) {
     if (!pcs[peerId]) {
         initializePeerConnection(peerId);
@@ -102,9 +102,9 @@ async function handleRemoteDescription(peerId, sdp) {
     const currentState = pcs[peerId].signalingState;
     console.log(`Current signaling state for peer ${peerId}:`, currentState);
 
-    // 연결 상태가 'stable'인 경우, SDP를 처리하지 않음
-    if (currentState === 'stable' && rtcSessionDescription.type === 'offer') {
-        console.log(`Peer ${peerId} is already connected. Ignoring offer.`);
+    // `stable` 상태인데 `answer`를 보내지 않은 경우, 무시하거나 적절하게 처리
+    if (currentState === 'stable' && rtcSessionDescription.type !== 'answer') {
+        console.log(`Peer ${peerId} is already connected. Ignoring SDP of type ${rtcSessionDescription.type}`);
         return;
     }
 
@@ -146,6 +146,7 @@ async function handleRemoteDescription(peerId, sdp) {
         console.error(`Error setting remote description for peer ${peerId}`, e);
     }
 }
+
 
 
 
@@ -210,6 +211,7 @@ async function setupWebSocket() {
             }
         }
     };
+    
     
     
 
